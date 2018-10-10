93602936e7e49fefa11876a3d67274d251b70a20
Sergio <sergioml88@gmail.com>
2018-10-10 13:10:50 +0200

    Docker compose deployment with nginx, mariadb and phpmyadmin

---
+++ web/public/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <div>SERVER ADDRESS = <?php echo $_SERVER['SERVER_ADDR'] ?></div>
+        <div>REMOTE ADDRESS = <?php echo $_SERVER['REMOTE_ADDR'] ?></div>
+    </body>
+</html>
